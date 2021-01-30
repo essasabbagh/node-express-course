fca0b38f0cfc2e03607bca3e3f32acf03c16da84
essasabbagh <essasabbagh@gmail.com>
2021-01-31 00:01:42 +0300

    basic middleware

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const bodyParser = require('bodyParser')
 const app = express();
-app.use(bodyParser.json())
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
 const mockUserData = [{ name: "essa" }, { name: "fadi" }];
+
+// executed every time the app receives a request.
+app.use(function (req, res, next) {
+  console.log("Time:", Date.now());
+  next();
+});
 
 app.get("/users", function (req, res) {
   res.json({
@@ -20,26 +28,26 @@ app.get("/users/:id", function (req, res) {
   });
 });
 
-app.post("/login", function(req,res){
-    const username = req.body.username
-    const password = req.body.passeord
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
 
-    const mockUsername = "bellyKid"
-    const mockPassword = "topsecret"
+  const mockUsername = "bellyKid";
+  const mockPassword = "topsecret";
 
-    if (username === mockUsername && password === mockPassword) {
-        res.json({
-            success:true,
-            message: 'username and password match',
-            token: 'encrypted token goes here'
-        })
-    } else {
-        res.json({
-            success: false,
-            message: 'username and password not match'
-        })
-    }
-})
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      success: true,
+      message: "username and password match",
+      token: "encrypted token goes here",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "username and password not match",
+    });
+  }
+});
 app.listen(8000, function () {
   console.log("Server is running on port: 8000");
 });
