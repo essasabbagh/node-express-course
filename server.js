2a221d4a806a3fd50de9fef5e2abdb78183aeb07
essasabbagh <essasabbagh@gmail.com>
2021-01-30 20:02:16 +0300

    add login POST route

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const bodyParser = require('bodyParser')
 const app = express();
-
+app.use(bodyParser.json())
 const mockUserData = [{ name: "essa" }, { name: "fadi" }];
 
-app.get("/user", function (req, res) {
+app.get("/users", function (req, res) {
   res.json({
     success: true,
     message: "Sucsessfuly got user, Nice!",
@@ -11,13 +12,34 @@ app.get("/user", function (req, res) {
   });
 });
 
-app.get("/user/:id", function (req, res) {
+app.get("/users/:id", function (req, res) {
   res.json({
     success: true,
     message: "got one user",
     user: req.params.id,
   });
 });
+
+app.post("/login", function(req,res){
+    const username = req.body.username
+    const password = req.body.passeord
+
+    const mockUsername = "bellyKid"
+    const mockPassword = "topsecret"
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success:true,
+            message: 'username and password match',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'username and password not match'
+        })
+    }
+})
 app.listen(8000, function () {
-  console.log("Server is running");
+  console.log("Server is running on port: 8000");
 });
